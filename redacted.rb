4fda96048fb788208056349ebadb281096c15321
Mestica Papna <m.papna@gmail.com>
2014-12-24 08:35:39 +0800

    Redacts a word from a text

---
+++ redacted.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+puts "Text to search through: "
+text = gets.chomp
+puts "Word to redact: "
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each do |word|
+    if word === redact
+        print "REDACTED "
+    else
+        print word + " "
+    end
+end
